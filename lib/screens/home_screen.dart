72c8707fda8c1896ce360222d1276f71a256df46
AvishekThapa1997 <avishekthapa1997@gmail.com>
2021-08-06 17:24:25 +0530

    product list added

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,23 +1,21 @@
-
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app/models/product.dart';
 import 'package:flutter_app/widgets/drawer.dart';
+import 'package:flutter_app/widgets/product_widget.dart';
 
-class HomeScreen extends StatelessWidget{
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-      return Scaffold(
-        appBar: AppBar(
-          title: Text("Catalog App")
-        ),
-        body: Center(
-          child: Container(
-            child: Text("Flutter Day-1"),
-          ),
-        ),
-        drawer: DrawerMenu(),
-      );
+    return Scaffold(
+      appBar: AppBar(title: Text("Catalog App")),
+      body: ListView.builder(
+        padding: EdgeInsets.all(16),
+        itemCount: CatalogueModel.items.length,
+        itemBuilder: (context, index) =>
+            ProductWidget(productItem: CatalogueModel.items[index]),
+      ),
+      drawer: DrawerMenu(),
+    );
   }
-
 }
---
+++ lib/widgets/product_widget.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_app/models/product.dart';
+
+class ProductWidget extends StatelessWidget {
+  final Product productItem;
+
+  const ProductWidget({Key? key, required this.productItem}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build;
+    return Card(
+      child: ListTile(
+        leading: Image.network(productItem.image!),
+        minLeadingWidth: 100,
+        title: Text(productItem.title!),
+        subtitle: Text(productItem.description!),
+        trailing: Text(
+          "\$ ${productItem.price.toString()}",
+          textScaleFactor: 1.5,
+          style: TextStyle(
+            color: Colors.deepPurple,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        onTap: () => {
+
+        },
+      ),
+    );
+  }
+}
